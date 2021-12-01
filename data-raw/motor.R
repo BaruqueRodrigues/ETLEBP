f036d1d56c6da049314bab7fef7e073ef9166107
BaruqueRodrigues <quemuel.baruque@ufpe.br>
2021-11-30 22:55:58 -0300

    atualizaçao

--- data-raw/motor.R
+++ data-raw/motor.R
@@ -1,17 +1,14 @@
-#### MOTOR ####
+    #### MOTOR ####
 ### construindo vetores com IEAs para contruir a variável ####
 iea1_1 <- c("motor",
-            "maquina eletrica",
+            "(maquina.*eletrica)",
             "equipamento",
-            "sistema eletrico",
+            "(sistema.*eletrico)",
             "aparelho", # 5
             "etiquetagem",
             "metrologia",
-            "processo industr", "eficiencia energetica", "eficiencia",
-            "injetora", #11
-            "processo",
-            "energ", "industr",
-            "refrigeracao" #15
+            "(processo.*industr)",
+            "(eficiencia.*energetica)"
 )
 
 iea1_2<- c("conforto termico",
@@ -30,12 +27,13 @@ iea1_2<- c("conforto termico",
            "etiquetagem" #14
 )
 
-iea1_3 <- c("bateria", "transporte",  "veiculo",
+iea1_3 <- c("(bateria.*transporte)",  "veiculo",
             "veiculo eletrico.",
-            "motor", "gasolina", "diesel", "etanol", "eficiente" # 9
+            "motor", "gasolina", "diesel",
+            "(etanol.*eficiente)" # 9
 )
 
-iea1_4 <- c("eficiencia","energetica")
+iea1_4 <- c("(eficiencia.*energetica)")
 
 iea2_1 <- c("petroleo",
             "oleo",
@@ -60,7 +58,8 @@ iea2_1 <- c("petroleo",
             "gasolina",
             "oleo diesel",
             "nafta", #23
-            "combustao", "petroleo", "gas natural", "derivado de petroleo", #27
+            "combustao", "petroleo",
+            "gas natural", "derivado de petroleo", #27
             "turbina a gas",
             "turbina a vapor",
             "turbina de avi.o",
@@ -70,40 +69,48 @@ iea2_1 <- c("petroleo",
             "tanques",
             "gasodutos",
             "gnl", #36
-            "armazenamento", "petroleo", "gas natural", "derivado de petroleo")
+            "(armazenamento.*petroleo)",
+            "gas natural", "derivado de petroleo")
 
 iea2_2 <- c("carvao",
             "carvao mineral",
-            "combustao", "carvao",
-            "conversao", "carvao")
+            "(combustao.*carvao)",
+            "(conversao.*carvao)")
 
-iea2_3 <- c("co2", "captura", "separacao",
-            "escoamento", "armazenamento")
+iea2_3 <- c("(captura.*CO2)", "(separacao.*CO2)",
+            "(escoamento.*CO2)",
+            "(armazenamento.*CO2)")
 
-iea3_1 <- c("solar",
-            "energia",
-            "aquecimento",
-            "arrefecimento",
+iea3_1 <- c("(energia.*solar)",
+            "(aquecimento.*solar)",
+            "(arrefecimento.*solar)",
             "termica",
-            "concentrador", #6
-            "fotovoltaica",
+            "(concentrador.*solar)", #6
+            "(sistema.*fotovoltaica)",
             "celula solar",
             "modulo fotovoltaico",
             "sistema fotovoltaico",
             "heliotermica",
+            "(solar.*termica)",
             "aplicao", "alta" , "temperatura")
 
-iea3_2 <- c("energia", "eolica",
-            "eolica", "onshore", "offshore", "turbina",
+iea3_2 <- c("(energia.*eolica)",
+            "(eolica.*onshore)", "(eolica.*offshore)",
+            "(turbina.*eolica)",
             "usinade vento",
             "rotor",
             "aerogerador",
-            "conversor")
+            "conversor",
+            "pas.*eolica")
 
-iea3_3 <- c("energia", "oce.nica", "mar.", "ondas",
-            "poder", "gradiente", "salinidade")
+iea3_3 <- c("energia.*oceanica",
+            "(energia.*mare)",
+            "(energia.*ondas)",
+            "mar.", "ondas",
+            "(poder.*gradiente.*salinidade)")
 
-iea3_4 <-c("biocombustivel", "liquido", "solido",
+iea3_4 <-c("(biocombustivel.*liquido)",
+           "(biocombustivel.*solido)",
            "biogas",
            "biomassa",
            "etanol",
@@ -116,11 +123,11 @@ iea3_4 <-c("biocombustivel", "liquido", "solido",
            "pirolise",
            "residuo",
            "glicerol", #15
-           "enzima", "hidrolise",
+           "(enzima.*hidrolise)",
            "hidrolise enzimatica",
-           "hidrolise", "acida", #20
+           "(hidrolise.*acida)", #20
            "lignocelulose",
-           "biocombustivel", "segunda","geracaoo", #24
+           "(biocombustivel.*segunda.*geracao)", #24
            "biodiesel",
            "oleos vegetais",
            "oleaginosa",
@@ -135,9 +142,9 @@ iea3_4 <-c("biocombustivel", "liquido", "solido",
            "biorreator")
 
 iea3_5 <- c("energia geotermica",
-            "energia", "recurso", "hidrotermal",
-            "energia", "recurso", "rocha", "seca", "quente",
-            "rocha", "perfuracao", "exploracao")
+            "(energia.*recurso.*hidrotermal)",
+            "(energia.*recurso.*rocha.*seca.*quente)",
+            "(rocha.*perfuracao)", "(rocha.*exploracao)")
 
 iea3_6 <- c("hidroeletricidade",
             "hidroeletrica",
@@ -145,39 +152,40 @@ iea3_6 <- c("hidroeletricidade",
             "uhe",
             "barragem")
 
-iea3_7 <- c("fontes", "energia", "renovavel")
+iea3_7 <- c("(fontes.*energia.*renovavel)")
 
 iea4_1 <- c("fissao nuclear",
             "uranio",
             "enriquecimento",
             "combustivel nuclear", #4
-            "reator", "nuclear",
+            "(reator.*nuclear)",
+            "(reatores.*nuclear)",
             "usina nuclear",
             "pwr",
             "bwr",
-            "nuclear","residuo", #11
-            "radioisotopos",
+            "(residuo.*nuclear)", #11
+            "(radioisotopos.*nuclear)",
             "regenardor nuclear")
 
-iea4_2 <- c("fusão","nuclear", "fusão nuclear")
+iea4_2 <- c("(fusão.*nuclear)", "fusão nuclear")
 
-iea4_9 <- c("energia","nuclear", "energia nuclear")
+iea4_9 <- c("(energia.*nuclear)", "energia nuclear")
 
 iea5_1 <- c("hidrogenio",
-            "producao", "armazenamento",
-            "transporte","distribuicao",
-            "infra-estrutura", "sistema")
+            "(producao.*hidrogenio)", "(armazenamento.*hidrogenio)",
+            "(transporte.*hidrogenio)","(distribuicao.*hidrogenio)",
+            "(infra-estrutura.*hidrogenio)", "(sistema.*hidrogenio)")
 
 iea5_2 <- c("celula a combustivel",
             "celula a combustao",
-            "aplicao", "estacionaria", "movel")
+            "(aplicao.*estacionaria)", "movel")
 
 iea6_1 <- c("geração de energia eletrica",
             "tecnologia de geracao de energia eletrica",
             "gerador de energia eletrica",
             "alternador",
             "cogeracao",
-            "caldeira","energia eletrica")
+            "(caldeira.*energia eletrica)")
 
 iea6_2 <- c("transmissao de eletricidade",
             "condutor",
@@ -187,16 +195,17 @@ iea6_2 <- c("transmissao de eletricidade",
             "rede de energia eletrica",
             "rede inteligente",
             "carga eletrica", # 8
-            "sistema","controle", "integracao",
+            "(sistema.*controle)", "integracao",
             "transformador", "alta tensao")
 
-iea6_3 <- c("armazenamento", "energia", "eletrico", "termica",
+iea6_3 <- c("(armazenamento.*energia)", "(armazenamento.*eletrico)",
+            "(armazenamento.*termica)",
             "bateria", "veiculo")
 
-iea7_1 <- c("energ", "sistema", "modelag","planejamento",
-            "algoritmo","energia","eletricidade")
+iea7_1 <- c("(sistema.*energ)", "(modelag.*planejamento)",
+            "(algoritmo.*nergia)","eletricidade")
 
-iea7_2 <- c("pesquisa","basica", "energia",
+iea7_2 <- c("(pesquisa.*basica.*energia)",
             "pesquisa energetica")
 
 
@@ -254,5 +263,6 @@ usethis::use_data(iea6_2, overwrite = TRUE)
 usethis::use_data(iea6_3, overwrite = TRUE)
 usethis::use_data(iea7_1, overwrite = TRUE)
 usethis::use_data(iea7_2, overwrite = TRUE)
+
 
 
